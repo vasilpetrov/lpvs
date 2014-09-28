bf90f880854471ae813f496bb57d0da61f5e2d69
Vasil Petrov <petrov.cisco@gmail.com>
2014-09-28 18:16:20 +0300

    adding lpvs script

--- manifests/init.pp
+++ manifests/init.pp
@@ -37,5 +37,51 @@
 #
 class lpvs {
 
+	 file { "/opt/lpvs":
+    		ensure => "directory",
+    		owner  => "root",
+    		group  => "root",
+    		mode   => 755,
+	 }
+
+         file { "/opt/lpvs/lpvs-scan.pl":
+                        ensure => file,
+                        owner  => 'root',
+                        group  => 'root',
+                        mode   => 0744,
+                        source => 'puppet:///modules/lpvs/lpvs-scan.pl',
+         }
+
+	if $osfamily == "RedHat" {
+	
+		package {"perl-XML-LibXML":
+			ensure => latest,
+		}
+
+		package {"perl-XML-LibXSLT":
+			ensure => latest,
+		}
+		
+		package { 'perl-Crypt-SSLeay':
+			ensure => latest,
+		}
+		
+		package {'perl-LWP-Protocol-https':
+			ensure => latest,
+		}	
+	
+	} elsif $osfamily == "Debian" {
+
+		 package { "libxml-libxslt-perl":
+    			ensure => "installed"
+		 }	
+	}
+
+	cron {'pkg_vuln_scan':
+                        command => "perl /opt/lpvs/lpvs-scan.pl -s > /opt/lpvs/scan_$hostname",
+                        user    => 'root',
+                        hour    => "2",
+                        minute  => "0",
+                     }
 
 }
